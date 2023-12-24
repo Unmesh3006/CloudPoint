5caa067137a2d98bb2774dbad27c8596ec7778b2
Unmesh3006 <89841143+Unmesh3006@users.noreply.github.com>
2023-12-24 16:02:24 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "353b27b45cf6f36747348c80313c00dd",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
